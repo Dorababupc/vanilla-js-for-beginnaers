2a220cd74fdfa80bea7f376ff15f5e7cd238a8e7
Dorababu Arigi <87510464+Dorababupc@users.noreply.github.com>
2021-12-07 16:53:57 +0530

    disaplaying the output of the api call

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var btntranslate= document.querySelector('#btn-translate');
 var txtarea=document.querySelector('#txt-area');
 var output=document.querySelector('#output');
-var input=txtarea.value;
-var baseURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var baseURL="https://api.funtranslations.com/translate/minion.json";
 function makeURL(input){
-    console.log(baseURL+"?"+"text="+"input");
-    return baseURL+"?"+"text="+"input";
+    
+    return baseURL+"?"+"text="+input;
 }
 function errorHandler(error){
     console.log("error occured ",error);
 }
 btntranslate.addEventListener("click",function clickHandler(){
-    fetch(makeURL(input))
+    console.log(txtarea.value)
+    fetch(makeURL(txtarea.value))
       .then(response => response.json())
-      .then(json => console.log(json.contents.translated))
+      .then(json => {
+          var translated=json.contents.translated;
+          output.innerText=translated;
+      })
       .catch(errorHandler)
 })
