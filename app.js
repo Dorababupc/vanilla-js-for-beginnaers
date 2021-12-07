eae12282a02a8749954d9291ffd0cee5c6d27da8
Dorababu Arigi <87510464+Dorababupc@users.noreply.github.com>
2021-12-07 10:54:42 +0530

    taking input and console.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var btntranslate= document.querySelector('#btn-translate');
+var txtarea=document.querySelector('#txt-area');
 btntranslate.addEventListener("click",function clickEventHandler(){
-    console.log("HII");
+    console.log(txtarea.value);
 })
